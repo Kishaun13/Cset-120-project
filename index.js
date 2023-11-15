82adc1c2fc2071b2fc3d55003eb4b2a20e8b81ba
Kishaun13 <Kishaunbrown14@gmail.com>
2023-11-15 12:08:20 -0500

    Added a footer to index.html login and sign-up pages added more functionality to the login page added more functionality to the sign-up page chnaged background images for pages

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-function signUp(){
+function signUp() {
     let email = document.getElementById("email").value;
     console.log(email);
     let pass = document.getElementById("pass").value;
     console.log(pass);
+    if (document.getElementById("email").value == "" || document.getElementById("pass").value == "") {
+        alert("Please fill the forms");
+    } else {
+        localStorage.setItem(email, pass);
+        alert("Account Created");
+        location.replace("login.html");
+    }
+
+
 
     localStorage.setItem(email, pass);
 }
 
-function login(){
+function login() {
     let email = document.getElementById("email").value;
     let pass = document.getElementById("pass").value;
 
-    if (localStorage.getItem(email)){
-        if (pass == localStorage.getItem(email)){
+    if (localStorage.getItem(email)) {
+        if (pass == localStorage.getItem(email)) {
             location.replace("menu.html");
-        }
-        else{
+        } else {
             alert("Login Failed");
         }
-    }
-    else{
+    } else {
         alert("Login Failed");
     }
 }
