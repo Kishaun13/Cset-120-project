2a07edb5e93dfa3dccc84ae3b021d36b8004d164
Kishaun13 <Kishaunbrown14@gmail.com>
2023-11-29 07:44:33 -0500

    added a missing bracket to menu.js

--- menu.js
+++ menu.js
@@ -1,34 +1,34 @@
 let cart = {};
 if (localStorage.getItem("cart")) {
-  cart = JSON.parse(localStorage.getItem("cart"));
-  displayCartItems();
-  updateCartTotal();
+    cart = JSON.parse(localStorage.getItem("cart"));
+    displayCartItems();
+    updateCartTotal();
 }
 
 function addToCart(itemName, itemPrice) {
-   itemName = document.getElementsByClassName('shop-item-title')
-  if (cart[itemName]) {
-    alert(`${itemName} is already in the cart!`);
-  } else {
-    cart[itemName] = { price: itemPrice, quantity: 1 };
-    console.log(cart);
-    displayCartItems();
-    updateCartTotal();
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }
+    itemName = document.getElementsByClassName('shop-item-title')
+    if (cart[itemName]) {
+        alert(`${itemName} is already in the cart!`);
+    } else {
+        cart[itemName] = { price: itemPrice, quantity: 1 };
+        console.log(cart);
+        displayCartItems();
+        updateCartTotal();
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
 }
 
 function displayCartItems() {
-  const cartItemsContainer = document.querySelector(".cart-items");
-  cartItemsContainer.innerHTML = "";
+    const cartItemsContainer = document.querySelector(".cart-items");
+    cartItemsContainer.innerHTML = "";
 
-  for ( itemName in cart) {
-    const item = cart[itemName];
+    for (itemName in cart) {
+        const item = cart[itemName];
 
-    const cartRow = document.createElement("div");
-    cartRow.classList.add("cart-row");
+        const cartRow = document.createElement("div");
+        cartRow.classList.add("cart-row");
 
-    const cartRowHTML = `
+        const cartRowHTML = `
         <div class="cart-item cart-column">
           <span class="cart-item-title">${itemName}</span>
         </div>
@@ -41,67 +41,69 @@ function displayCartItems() {
         </div>
       `;
 
-    cartRow.innerHTML = cartRowHTML;
-    cartItemsContainer.append(cartRow);
-  }
+        cartRow.innerHTML = cartRowHTML;
+        cartItemsContainer.append(cartRow);
+    }
 
-  let quantityInputs = document.querySelectorAll(".cart-quantity-input");
-  quantityInputs.forEach((input) => {
-    input.addEventListener("change", (event) => {
-      let newQuantity = parseInt(event.target.value);
-      let itemName = event.target.getAttribute("data-item-name");
-      console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
-      updateQuantity(itemName, newQuantity);
+    let quantityInputs = document.querySelectorAll(".cart-quantity-input");
+    quantityInputs.forEach((input) => {
+        input.addEventListener("change", (event) => {
+            let newQuantity = parseInt(event.target.value);
+            let itemName = event.target.getAttribute("data-item-name");
+            console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
+            updateQuantity(itemName, newQuantity);
+        });
     });
-  });
 }
+
 function removeFromCart(itemName) {
-  delete cart[itemName];
-  displayCartItems();
-  updateCartTotal();
-  localStorage.setItem("cart", JSON.stringify(cart));
+    delete cart[itemName];
+    displayCartItems();
+    updateCartTotal();
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 function updateCartTotal() {
-  let total = 0;
-  for (let itemName in cart) {
-    total += cart[itemName].price * cart[itemName].quantity;
-  }
+    let total = 0;
+    for (let itemName in cart) {
+        total += cart[itemName].price * cart[itemName].quantity;
+    }
 
-  document.querySelector(".cart-total-price").innerText =
-    "$" + total.toFixed(2);
+    document.querySelector(".cart-total-price").innerText =
+        "$" + total.toFixed(2);
 }
 
 function clearCart() {
-  cart = {};
-  localStorage.removeItem("cart");
-  displayCartItems();
-  updateCartTotal();
+    cart = {};
+    localStorage.removeItem("cart");
+    displayCartItems();
+    updateCartTotal();
 }
 
 function updateQuantity(itemName, quantity) {
-  if (cart[itemName]) {
-    cart[itemName].quantity = quantity;
-    updateCartTotal();
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }
+    if (cart[itemName]) {
+        cart[itemName].quantity = quantity;
+        updateCartTotal();
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
 }
 let quantityInputs = document.querySelectorAll(".cart-quantity-input");
 quantityInputs.forEach((input) => {
-  input.addEventListener("change", (event) => {
-    let newQuantity = parseInt(event.target.value);
-    let itemName = event.target.getAttribute("data-item-name");
-    console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
-    updateQuantity(itemName, newQuantity);
-  });
+    input.addEventListener("change", (event) => {
+        let newQuantity = parseInt(event.target.value);
+        let itemName = event.target.getAttribute("data-item-name");
+        console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
+        updateQuantity(itemName, newQuantity);
+    });
 });
 
 function updateQuantity(itemName, quantity) {
-  if (cart[itemName]) {
-    cart[itemName].quantity = quantity;
-    updateCartTotal();
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }
+    if (cart[itemName]) {
+        cart[itemName].quantity = quantity;
+        updateCartTotal();
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+}
 
 
 
