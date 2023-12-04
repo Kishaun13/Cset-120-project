b28b8c166a51adc595263844ee5bc36f41eec948
Kishaun13 <Kishaunbrown14@gmail.com>
2023-12-04 10:54:17 -0500

    fixed cart not staying the same between all pages added a line to payement.html to show the total added lines to display the cart items on payment.html added a function to generate and display the receipt on receipt.html created a fucntion returnHome to that resets the cart abd replaces the current page with index.html that is withi a button on receipt.html

--- menu.js
+++ menu.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-
-  document.getElementById('menus').contentEditable = false
-
+    document.getElementById('menus').contentEditable = false
 }
 let cart = {};
 if (localStorage.getItem("cart")) {
@@ -9,22 +7,12 @@ if (localStorage.getItem("cart")) {
     displayCartItems();
     updateCartTotal();
 }
-// let cart = JSON.parse(localStorage.getItem('cartarray'))??[];
-// // if (localStorage.getItem("cart")) {
-// //     // cart = JSON.parse(localStorage.getItem("cart"));
-// //     displayCartItems();
-// //     updateCartTotal();
-// // }
 
 function addToCart(itemName, itemPrice) {
-
-
-
     if (cart[itemName]) {
         alert(`${itemName} is already in the cart!`);
     } else {
         cart[itemName] = { price: itemPrice, quantity: 1 };
-        // localStorage.setItem('cart', JSON.stringify(cart))
         displayCartItems();
         updateCartTotal();
         localStorage.setItem("cart", JSON.stringify(cart));
@@ -32,44 +20,33 @@ function addToCart(itemName, itemPrice) {
     }
 }
 
-
 function displayCartItems() {
     const cartItemsContainer = document.querySelector(".cart-items");
     cartItemsContainer.innerHTML = "";
-
-
     for (let itemName in cart) {
         const item = cart[itemName];
-        //     for (itemName in cart) {
-        //         const item = cart[itemName];
-
-
         const cartRow = document.createElement("div");
         cartRow.classList.add("cart-row");
-
         const cartRowHTML = `
         <div class="cart-item cart-column">
           <span class="cart-item-title">'${itemName}'</span>
         </div>
         <span class="cart-price cart-column">$${item.price.toFixed(2)}</span>
         <div class="cart-quantity cart-column">
-          <input class="cart-quantity-input" type="number" value="${
+          <input class="cart-quantity-input" type="number" min = 1 value="${
             item.quantity
           }" data-item-name="${itemName}">
           <button onclick="removeFromCart('${itemName}')" class="btn btn-danger">REMOVE</button>
         </div>
       `;
-
         cartRow.innerHTML = cartRowHTML;
         cartItemsContainer.append(cartRow);
     }
-
     let quantityInputs = document.querySelectorAll(".cart-quantity-input");
     quantityInputs.forEach((input) => {
         input.addEventListener("change", (event) => {
             let newQuantity = parseInt(event.target.value);
             let itemName = event.target.getAttribute("data-item-name");
-            console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
             updateQuantity(itemName, newQuantity);
         });
     });
@@ -87,7 +64,6 @@ function updateCartTotal() {
     for (let itemName in cart) {
         total += cart[itemName].price * cart[itemName].quantity;
     }
-
     document.querySelector(".cart-total-price").innerText =
         "$" + total.toFixed(2);
 }
@@ -111,59 +87,64 @@ quantityInputs.forEach((input) => {
     input.addEventListener("change", (event) => {
         let newQuantity = parseInt(event.target.value);
         let itemName = event.target.getAttribute("data-item-name");
-        console.log(`Item Name: ${itemName}, New Quantity: ${newQuantity}`);
         updateQuantity(itemName, newQuantity);
     });
 });
 
-function updateQuantity(itemName, quantity) {
-    if (cart[itemName]) {
-        cart[itemName].quantity = quantity;
-        updateCartTotal();
-        localStorage.setItem("cart", JSON.stringify(cart));
-    }
-}
-
-
-
 function purchaseProducts() {
-    let completecart = document.getElementById('cart')
-    localStorage.setItem("cart", completecart.outerHTML);
     if (Object.keys(cart).length === 0) {
         alert("Your cart is empty!");
     } else {
+        localStorage.setItem("cart", JSON.stringify(cart));
         location.replace("payment.html");
+        alert("Thank you for ordering!");
     }
 }
 
-
-//Payment page
-
-function openForm1(){
+function openForm1() {
     document.getElementById('forms').style.display = "block"
     document.getElementById('cardcredit').style.display = "none"
     document.getElementById('cash').style.display = "block"
 };
-function openForm2(){
+
+function openForm2() {
     document.getElementById('forms').style.display = "block"
     document.getElementById('cardcredit').style.display = "block"
     document.getElementById('cash').style.display = "none"
 };
-function closeForm1(){
+
+function closeForm1() {
     document.getElementById('forms').style.display = "none";
     alert("Thank you for your purchase!")
     window.location.assign("/receipt.html")
 };
-function closeForm2(){
+
+function closeForm2() {
     document.getElementById('forms').style.display = "none"
     alert("Thank you for your purchase!")
     window.location.assign("/receipt.html")
 };
-function back(){
+
+function back() {
     document.getElementById('forms').style.display = "none"
 }
 
-
-// var storedcart = localStorage.getItem('cart')
-// // console.log('storedcart')
-// document.getElementById('itemsincart').innerHTML = storedcart
+function generateReceipt() {
+    let receipt = 'Receipt\n';
+    receipt += '----------------------\n';
+    for (let itemName in cart) {
+        const item = cart[itemName];
+        receipt += `${itemName}: $${item.price.toFixed(2)} x ${item.quantity} = $${(item.price * item.quantity).toFixed(2)}\n`;
+    }
+    const total = Object.values(cart).reduce((acc, item) => acc + item.price * item.quantity, 0);
+    receipt += '----------------------\n';
+    receipt += `Total: $${total.toFixed(2)}\n`;
+    receipt += '----------------------\n';
+    receipt += 'Thank you for your purchase!\n';
+    localStorage.setItem('receipt', receipt);
+    window.location.href = 'receipt.html';
+}
+localStorage.setItem('receipt', receipt);
+setTimeout(function() {
+    window.location.href = 'receipt.html';
+}, 500);
