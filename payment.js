8c442b130ea8b6e8bca41c8bda3360edadbd43e8
Kishaun13 <Kishaunbrown14@gmail.com>
2023-12-04 07:30:56 -0500

    commented out unneeded lines in payment.js and changed the name of items in cart div to cart iems

--- payment.js
+++ payment.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-    let totalPrice = localStorage.getItem('totalPrice');
-    if (totalPrice) {
-        document.querySelector(".total-price").innerText = "$" + parseFloat(totalPrice).toFixed(2);
-    }
-}
+// window.onload = function() {
+//     let totalPrice = localStorage.getItem('totalPrice');
+//     if (totalPrice) {
+//         document.querySelector(".total-price").innerText = "$" + parseFloat(totalPrice).toFixed(2);
+//     }
+// }
